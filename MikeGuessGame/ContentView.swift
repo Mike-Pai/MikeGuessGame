c51ae1f782490f3bc20c79b559916018f1bab4e9
MikePai <ye518612@gmail.com>
2021-03-05 18:01:51 +0800

    Initial Commit

---
+++ MikeGuessGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MikeGuessGame
+//
+//  Created by 白謹瑜 on 2021/3/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MikeGuessGame/MikeGuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MikeGuessGameApp.swift
+//  MikeGuessGame
+//
+//  Created by 白謹瑜 on 2021/3/5.
+//
+
+import SwiftUI
+
+@main
+struct MikeGuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
